21e03991791d815c23ff7edf1dfa1b09a0854b75
Dennis <undno@student.kit.edu>
2013-11-15 19:14:03 +0100

    Klasse LaufzeitAnalysator Berechnungsfunktion angefangen

--- p-it/LaufzeitAnalysator.h
+++ p-it/LaufzeitAnalysator.h
@@ -1,8 +1,17 @@
-#pragma once
+#ifndef LAUFZEITANALYSATOR_H
+#define LAUFZEITANALYSATOR_H
+
+#include "Faktoren.h"
+#include "Bibliothek.h"
+#include "SchaltwerkElement.h"
+
 class LaufzeitAnalysator
 {
 public:
 	LaufzeitAnalysator();
 	~LaufzeitAnalysator();
+	void berechnungLaufzeitEinzelgatter(void);
+
 };
 
+#endif
